3ca291f60d71bccbfb30ce34f3399e765a9a12e1
George G <grreulvi@gmail.com>
2024-11-04 01:28:02 +0300

    [module4-task1] change: add user inteface to export in types index

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -2,3 +2,4 @@ export { Component } from './component.enum.js';
 export { JsonServerData } from './json-server-data.type.js';
 export { OfferType } from './offer-type.enum.js';
 export { Offer } from './offer.type.js';
+export { User } from './user.type.js';
