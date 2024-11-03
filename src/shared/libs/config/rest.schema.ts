e8bf33bc7435963f12b00b670523aa5e40b1f889
George G <grreulvi@gmail.com>
2024-11-04 00:08:49 +0300

    [module4-task1] change schema, added db user, password and port

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -7,6 +7,9 @@ export type RestSchema = {
   PORT: number;
   SALT: string;
   DB_HOST: string;
+  DB_USER: string;
+  DB_PASSWORD: string;
+  DB_PORT: number;
 }
 
 export const configRestSchema = convict<RestSchema>({
@@ -27,5 +30,23 @@ export const configRestSchema = convict<RestSchema>({
     format: 'ipaddress',
     env: 'DB_HOST',
     default: '127.0.0.1'
-  }
+  },
+  DB_USER: {
+    doc: 'User used to connect to database',
+    format: String,
+    env: 'DB_USER',
+    default: null
+  },
+  DB_PASSWORD: {
+    doc: 'Password used to connect to database',
+    format: String,
+    env: 'DB_PASSWORD',
+    default: null
+  },
+  DB_PORT: {
+    doc: 'Port number used to connect to database',
+    format: 'port',
+    env: 'DB_PORT',
+    default: null
+  },
 });
