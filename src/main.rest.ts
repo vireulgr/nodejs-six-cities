fb4c9083ac3026651a48837a1c8ae38a311349ea
George G <grreulvi@gmail.com>
2024-11-04 00:28:54 +0300

    [module4-task1] change: add database client to IoC container

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -5,6 +5,7 @@ import { Component } from './shared/types/component.enum.js';
 import { RestApplication } from './rest/rest.application.js';
 import { Config, RestSchema, RestConfig } from './shared/libs/config/index.js';
 import { Logger, PinoLogger } from './shared/libs/logger/index.js';
+import { DatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
 
 
 async function bootstrap() {
@@ -12,6 +13,7 @@ async function bootstrap() {
   container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
   container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
+  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
 
   const application = container.get<RestApplication>(Component.RestApplication);
   await application.init();
